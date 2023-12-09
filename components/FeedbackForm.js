19f8ec75edc2c68886e4531fc0b91b20400e1e54
William Pan <williamlpan22@gmail.com>
2023-12-09 13:38:35 -0800

    Contact Form (added css modules, edited text and dropdown)

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -17,14 +17,30 @@ export default function FeedbackForm() {
             </label>
         </p>
   
-        <label htmlFor="name">Name</label>
-        <input id="name" className={styles['form-field']} type="text" name="name" />
+        <label htmlFor="full-name">Full Name</label>
+        <input id="full-name" className={styles['form-field']} type="text" name="full-name" required />
 
-        <label htmlFor="email">Email</label>
-        <input id="email" className={styles['form-field']} type="email" name="email" required />
+        <label htmlFor="company-name">Company Name</label>
+        <input id="company-name" className={styles['form-field']} type="text" name="company-name" required />
 
-        <label htmlFor="feedback">What is your feedback?</label>
-        <textarea id="feedback" className={styles['form-field']} wrap="soft" name="feedback" required></textarea>
+        <label htmlFor="company-email">Business Email</label>
+        <input id="company-email" className={styles['form-field']} type="text" name="company-email" required/>
+
+        <label htmlFor="phone-number">Phone Number</label>
+        <input id="phone-number" className={styles['form-field']} type="text" name="phone-number" required/>
+
+        <label htmlFor="job-title">Role/Position</label>
+        <input id="job-title" className={styles['form-field']} type="text" name="job-title" required/>
+
+        <label htmlFor="how-did-you-hear">How Did You Hear About Us?</label>
+        <select id="how-did-you-hear" className={styles['form-field']} type="dropdown" name="how-did-you-hear" required>
+            <option value="search-engine">Search Engine</option>
+            <option value="friend">Recommended by a friend or colleague</option>
+            <option value="social-media">Social Media</option>
+            <option value="blog">Blog or Publication</option>
+            <option value="other">Other</option>
+        </select>
+
         <button className={styles.button} type="submit">Submit</button>
       </form>
   )
--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,39 @@ import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 import FeedbackForm from "@components/FeedbackForm";
-import JokeBlock from "@components/JokeBlock";
+import styles from '../components/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>IntegrityAI Contact Form</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Next.js Toolbox" />
+      <main className={styles.main}>
+        <Header title="Meet With Our Business Team!" />
         <hr />
-        <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
-          submissions can be found in the Netlify Admin site.
-        </p>
-        <FeedbackForm />
-        <JokeBlock />
+        <div className={styles.contentWrapper}>
+          <section className={styles.textContent}>
+            <h2 className={styles.subHeading}>See How IntegrityAI Can Work For You!</h2>
+            <p className={styles.textItem}>
+              We're aiming to have institutions in all 50 states partner with IntegrityAI to ensure authenticity in writing. 
+            </p>
+            <p className={styles.textItem}>
+              Fill out this form to discover what IntegrityAI can do for you.
+            </p>
+            <p className={styles.subHeading}>
+              Are you a IntegrityAI user looking for support? 
+            </p>
+            <p className={styles.textItem}>
+              Check out our <a href="https://integrityai.app/#FAQ" target="_blank" rel="noopener noreferrer">FAQ!</a>
+            </p>
+          </section>
+          <div className={styles.formSection}>
+            <FeedbackForm />
+          </div>
+        </div>  
       </main>
       <Footer />
     </div>
